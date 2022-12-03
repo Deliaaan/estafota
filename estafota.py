83cae29cf1712776bdc687a6c50f833ec81aaa79
Deliaaan <delian.ngtmr@gmail.com>
2022-12-02 19:01:08 -0600

    deberia servir en linux xd

--- estafota.py
+++ estafota.py
@@ -1,1 +1,11 @@
-print("hello word")
+import time
+
+from selenium import webdriver
+from selenium.common.keys import keys
+
+driver = webdriver.Firefox()
+driver.get('https://www.estafeta.com/Herramientas/Rastreo')
+
+
+driver.refresh()
+time.sleep(5)
